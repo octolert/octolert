c5d6f90135d2b309ee2ebed3c770acb89f0843df
Lloyd Baxter <lloydbax@gmail.com>
2019-03-17 12:35:35 +0000

    Auto stash before merge of "master" and "origin/master"

--- src/routes/auth-globoards-router.js
+++ src/routes/auth-globoards-router.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const axios = require('axios');
 
 const getRouter = (options) => {
   const router = express.Router();
 
   router.route('/auth/globoards')
     .get((req, res) => {
-      // Save integration
       const integration = {
         name: 'globoards',
         app: 'globoards',
@@ -16,19 +16,29 @@ const getRouter = (options) => {
         },
       };
       options.integrationsService.addItem(integration);
-      // TODO: Redirect to oauth login for user
-      res.status('200').send({
-        setting1: 'hello world 2',
-      });
+      res.redirect(`https://app.gitkraken.com/oauth/authorize?response_type=code&client_id=${integration.attributes.clientId}&scope=board:read`);
     });
 
   router.route('/auth/globoards/token')
     .get((req, res) => {
-      // TODO: GET TOKEN FROM REQUEST
-      options.integrationsService.saveToken({ name: 'globoards', token: 'getfromreq' });
-      // TODO: Redirect user to page saying sucessful to setup trigger/alerts
-      res.status('200').send({
-        setting1: 'hello world 2',
+      const { code } = req.query;
+      const integration = options.integrationsService.getItem('globoards');
+      axios({
+        method: 'post',
+        url: 'https://api.gitkraken.com/oauth/access_token',
+        data: {
+          grant_type: 'authorization_code',
+          client_id: integration.attributes.clientId,
+          client_secret: integration.attributes.secret,
+          code,
+        },
+      }).then((response) => {
+        console.log(response);
+        const accessToken = response.data.access_token;
+        options.integrationsService.saveToken({ name: 'globoards', token: accessToken });
+        res.status('200').redirect('?message=globoard app integrated successfully');
+      }).catch((err) => {
+        console.log(err);
       });
     });
 
