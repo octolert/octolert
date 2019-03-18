f089b2b47f0a2e1f6c3a2b31ee69ce81b8f35f70
matthewbill <matthewdbill@gmail.com>
2019-03-18 23:57:03 +0000

    added persistent storage

--- src/trigger-alerts/triggers-alerts-respository.js
+++ src/trigger-alerts/triggers-alerts-respository.js
@@ -1,48 +1,75 @@
+const Datastore = require('nedb');
+const os = require('os');
+
 class TriggerAlertsRepository {
   constructor() {
     const self = this;
-    self.items = [];
+    const homeDir = os.homedir();
+    const path = `${homeDir}/octolert/trigger-alerts.json`;
+    self.db = new Datastore({ filename: path, autoload: true });
+    self.db.loadDatabase();
   }
 
-  getItems(options) {
+  getItems() {
     const self = this;
-    return self.items;
+    return new Promise((resolve, reject) => {
+      self.db.find({}, (err, docs) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(docs);
+        }
+      });
+    });
   }
 
   getItem(id) {
     const self = this;
-    for (let i = 0; i < self.items.length; i += 1) {
-      const item = self.items[i];
-      if (item.id === id) {
-        return item;
-      }
-    }
-    return null;
+    return new Promise((resolve, reject) => {
+      self.db.find({ id }, (err, docs) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(docs[0]);
+        }
+      });
+    });
   }
 
   addItem(entity) {
     const self = this;
-    self.items.push(entity);
+    return new Promise((resolve, reject) => {
+      self.db.insert(entity, (err) => {
+        if (err) {
+          reject(err);
+        }
+        resolve();
+      });
+    });
   }
 
   updateItem(entity) {
     const self = this;
-    for (let i = 0; i < self.items.length; i += 1) {
-      const item = self.items[i];
-      if (item.id === entity.id) {
-        self.items[i] = entity;
-      }
-    }
+    return new Promise((resolve, reject) => {
+      self.db.update({ id: entity.id }, entity, (err, numUpdate) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(numUpdate);
+      });
+    });
   }
 
-  deleteItem(entity) {
+  deleteItem(id) {
     const self = this;
-    for (let i = 0; i < self.items.length; i += 1) {
-      const item = self.items[i];
-      if (item.id === entity.id) {
-        self.items.splice(i, 1);
-      }
-    }
+    return new Promise((resolve, reject) => {
+      self.db.remove({ id }, {}, (err, numRemove) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(numRemove);
+      });
+    });
   }
 }
 
