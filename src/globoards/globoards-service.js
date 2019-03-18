a3863f6329d0d37c92959647079ed0c230a77893
Lloyd Baxter <lloydbax@gmail.com>
2019-03-18 20:56:07 +0000

    boards-columns-cards

--- src/globoards/globoards-service.js
+++ src/globoards/globoards-service.js
@@ -1,4 +1,6 @@
 const GloSDK = require('@axosoft/glo-sdk');
+const a = require('axios');
+
 /* eslint-disable no-empty-function */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-unused-vars */
@@ -18,15 +20,26 @@ class GloboardsService {
 
   async getColumnsForBoard(options) {
     try {
-      const columns = await GloSDK(options.token).boards.get('5beb506267f0ab000e7d4659', {});
+      const columns = await GloSDK(options.token).boards.get(options.boardId, { fields: ['columns'] });
       return columns;
     } catch (error) {
       throw error;
     }
   }
 
-  getCardsForBoard(options) {
-
+  async getCardsForBoard(options) {
+    try {
+      const axios = a.create({
+        baseURL: 'https://gloapi.gitkraken.com/v1/',
+        headers: {
+          Authorization: options.token,
+        },
+      });
+      const cards = await axios.get(`glo/boards/${options.boardId}/cards`);
+      return cards.data;
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
--- src/routes/integrations/globoards-router.js
+++ src/routes/integrations/globoards-router.js
@@ -5,37 +5,45 @@ const getRouter = (options) => {
 
   router.route('/integrations/globoards/boards')
     .get((req, res) => {
-      const integration = options.integrationsService.getItem('globoards');
-      options.globoardsService.getBoards({
-        token: integration.attributes.token,
-      }).then((response) => {
-        res.status('200').send(response);
-      }).catch((err) => {
-        console.log(err);
+      options.integrationsService.getItem('globoards').then((resp) => {
+        options.globoardsService.getBoards({
+          token: resp.attributes.token,
+        }).then((response) => {
+          res.status('200').send(response);
+        }).catch((err) => {
+          console.log(err);
+        });
       });
     });
 
   router.route('/integrations/globoards/columns')
     .get((req, res) => {
-      const integration = options.integrationsService.getItem('globoards');
-      options.globoardsService.getColumnsForBoard({
-        token: integration.attributes.token,
-        boardId: req.query.boardId,
-      }).then((response) => {
-        res.status('200').send(response);
-      }).catch((err) => {
-        console.log(err);
-      });
+      options.integrationsService.getItem('globoards')
+        .then((result) => {
+          options.globoardsService.getColumnsForBoard({
+            token: result.attributes.token,
+            boardId: req.query.boardId,
+          }).then((response) => {
+            res.status('200').send(response);
+          }).catch((err) => {
+            console.log(err);
+          });
+        });
     });
 
   router.route('/integrations/globoards/cards')
     .get((req, res) => {
-      const integration = options.integrationsService.getItem('globoards');
-      const cards = options.globoardsService.getCardsForBoard({
-        token: integration.attributes.token,
-        boardId: req.query.boardId,
-      });
-      res.status('200').send(cards);
+      options.integrationsService.getItem('globoards')
+        .then((result) => {
+          options.globoardsService.getCardsForBoard({
+            token: result.attributes.token,
+            boardId: req.query.boardId,
+          }).then((response) => {
+            res.status('200').send(response);
+          }).catch((err) => {
+            console.log(err);
+          });
+        });
     });
 
   return router;
