b71004a427543af0354033264559804443d92fbd
datbhatt@gmail.com <datbhatt@gmail.com>
2019-03-26 23:28:37 +0000

    added more functionality to trigger-config.jsx still WIP

--- src/js/components/trigger-config.jsx
+++ src/js/components/trigger-config.jsx
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class TriggerConfig extends Component {
-  //TODO JD WIP
+  // TODO JD WIP
   constructor(props) {
     super(props);
     this.state = {
       errorSaving: null,
       isLoaded: false,
-      data: null
+      data: null,
+      boards: null,
+      columns: null,
+      selectedBoardId: null,
+      selectedColumnId: null,
+      positiveMsg:null,
+      negativeMsg:null,
     };
+    this.getColumnsForBoard = this.getColumnsForBoard.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.setColumnId = this.setColumnId.bind(this);
   }
 
   componentDidMount() {
+    axios.get('integrations/globoards/boards').then((response) => {
+      this.setState({
+        boards: response.data,
+      });
+      this.getColumnsForBoard(response.data[0].id);
+    }, () => {
+      this.setState({
+        boards: null,
+      });
+    });
+
     fetch('/api/triggers')
       .then(res => res.json())
       .then(
@@ -30,42 +52,141 @@ class TriggerConfig extends Component {
       );
   }
 
+  getColumnsForBoard(boardId) {
+    this.setState({
+      selectedBoardId: boardId,
+    });
+
+    axios.get('integrations/globoards/columns', {
+      params: {
+        boardId,
+      },
+    }).then((response) => {
+      this.setState({
+        columns: response.data.columns,
+        selectedColumnId: response.data.columns[0].id,
+      });
+    }, () => {
+      this.setState({
+        columns: null,
+      });
+    });
+  }
+
+  setColumnId(event) {
+    this.setState({ selectedColumnId: event.target.value });
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    const {
+      selectedBoardId,
+      selectedColumnId,
+      positiveMsg,
+      negativeMsg,
+    } = this.state;
+
+    axios.all([
+      axios.post('/api/triggers', {
+        integrationId: 'globoards',
+        SourceId: selectedBoardId,
+        type: 'globoard-cardback',
+        attributes: [
+          { columnId: selectedColumnId },
+        ],
+      }),
+      axios.post('/api/triggers', {
+        integrationId: 'globoards',
+        SourceId: selectedBoardId,
+        type: 'globoard-cardforward',
+        attributes: [
+          { columnId: selectedColumnId },
+        ],
+      }),
+    ]).then(axios.spread((firstResponse, secondResponse) => {
+      axios.all([
+        axios.post('/api/triggeralerts', {
+          triggerId: firstResponse.data.id,
+          type: 'say',
+          attributes: [
+            { text: negativeMsg },
+          ],
+        }),
+        axios.post('/api/triggeralerts', {
+          triggerId: secondResponse.data.id,
+          type: 'say',
+          attributes: [
+            { text: positiveMsg },
+          ],
+        }),
+      ]).then(axios.spread((firstAlertResponse, secondAlertResponse) => {
+        // final
+      })).catch(() => {
+        // error
+      });
+    })).catch(() => {
+      // error again
+    });
+    event.preventDefault();
+  }
+
   render() {
+    const {
+      boards,
+      selectedBoardId,
+      columns,
+      selectedColumnId,
+      positiveMsg,
+      negativeMsg,
+    } = this.state;
+
     return (
-      <div>
+      <form onSubmit={this.handleSubmit}>
         <h4>Add/Edit Custom Alerts</h4>
         <p className="secondary">Apply alerts for specific columns. (this page is still under development)</p>
         <div className="row">
+          { boards !== null
+          && (
           <div className="col-xs-3">
             <div className="input-group">
               <label>Boards</label>
-              <select>
-                <option value="TEST-BOARD">Test board</option>
+              <select value={selectedBoardId} onChange={this.getColumnsForBoard}>
+                {boards.map(board => <option key={board.id} value={board.id}>{board.name}</option>)}
               </select>
             </div>
           </div>
-          <div className="col-xs-3">
-            <div className="input-group">
-              <label>Column</label>
-              <select>
-                <option value="todo">Todo</option>
-                <option value="in-progress">In-progress</option>
-                <option value="done">Done</option>
-              </select>
-            </div>
-          </div>
+          )
+          }
+          {
+            columns !== null
+            && (
+              <div className="col-xs-3">
+                <div className="input-group">
+                  <label>Column</label>
+                  <select value={selectedColumnId} onChange={this.setColumnId}>
+                    {columns.map(column => <option key={column.id} value={column.id}>{column.name}</option>)}
+                  </select>
+                </div>
+              </div>
+            )
+          }
         </div>
         <div className="input-group">
           <label>Postive message</label>
-          <textarea rows="6" type="text" />
+          <textarea rows="6" value={positiveMsg} id="positiveMsg" name="positiveMsg" onChange={this.handleChange} type="text" />
         </div>
         <div className="input-group">
           <label>Negative message</label>
-          <textarea rows="6" type="text" />
+          <textarea rows="6" value={negativeMsg} id="negativeMsg" name="negativeMsg" onChange={this.handleChange} type="text" />
         </div>
-        <button type="button" className="button-primary">Save Changes</button>
+        <button type="submit" className="button-primary">Save Changes</button>
         <br />
-      </div>
+      </form>
     );
   }
 }
